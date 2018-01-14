c4eda448f23e12ada1ac9ba68975745cc15e86d4
Alexandre Chaintreuil <alexandre.chaintreuil@gmail.com>
2018-01-14 12:27:26 +0100

    set up a REPL

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+use std::io::{self, Write};
+
+
+fn print_prompt() {
+    print!("db > ");
+    io::stdout().flush().unwrap();
+}
+
+fn read_input() -> String {
+    let mut input_buffer = String::new();
+
+    io::stdin()
+        .read_line(&mut input_buffer)
+        .expect("Failed to read stdin");
+
+    input_buffer
+}
+
 fn main() {
-    println!("Hello, world!");
+    loop {
+        print_prompt();
+        let input_buffer = read_input();
+
+        match input_buffer.trim().as_ref() {
+            ".exit" => std::process::exit(0),
+            _ => println!("Cannot process this input"),
+        }
+    }
 }
