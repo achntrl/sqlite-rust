27fc6bcfa6b2ef672c0deb228163e84bf27d067c
Alexandre Chaintreuil <alexandre.chaintreuil@gmail.com>
2018-01-14 15:44:25 +0100

    repl detects insert and select statements

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,31 @@
 use std::io::{self, Write};
 
+enum MetaCommandResult {
+    Success,
+    UnrecognizedCommand,
+}
+
+enum StatementType {
+    Insert,
+    Select,
+}
+
+struct Statement {
+    statement_type: StatementType,
+}
+
+static HELP_TEXT: &str = "
+This is an SQLite clone written in Rust.
+
+Available meta commands:
+    .exit : Quit the interactive shell
+    .help : Display this help message
+
+Available statements:
+    insert : Add a record in the database
+    select : Display records from the database
+";
+
 
 fn print_prompt() {
     print!("db > ");
@@ -16,14 +42,56 @@ fn read_input() -> String {
     input_buffer
 }
 
+fn execute_meta_command(input_buffer: String) -> MetaCommandResult {
+    match input_buffer.trim().as_ref() {
+        ".exit" => {
+            std::process::exit(0);
+        }
+        ".help" => {
+            println!("{}", HELP_TEXT);
+            MetaCommandResult::Success
+        }
+        _ => {
+            println!("Cannot execute this meta command");
+            MetaCommandResult::UnrecognizedCommand
+        }
+    }
+}
+
+fn prepare_statement(input_buffer: String) -> Option<Statement> {
+    let s: &str = input_buffer.trim().as_ref();
+    match s {
+        _ if s.starts_with("insert") => Some(Statement { statement_type: StatementType::Insert }),
+        _ if s.starts_with("select") => Some(Statement { statement_type: StatementType::Select }),
+        _ => None,
+    }
+}
+
+fn execute_statement(statement: Statement) {
+    match statement.statement_type {
+        StatementType::Insert => println!("This is where we execute the insert statement"),
+        StatementType::Select => println!("This is where we execute the select statement"),
+    }
+}
+
 fn main() {
     loop {
         print_prompt();
-        let input_buffer = read_input();
+        let mut input_buffer = read_input();
+        input_buffer = input_buffer.trim().to_string();
 
-        match input_buffer.trim().as_ref() {
-            ".exit" => std::process::exit(0),
-            _ => println!("Cannot process this input"),
+        if input_buffer.chars().next() == Some('.') {
+            match execute_meta_command(input_buffer) {
+                MetaCommandResult::Success => continue,
+                MetaCommandResult::UnrecognizedCommand => continue,
+            }
+        }
+
+        let statement = prepare_statement(input_buffer);
+
+        match statement {
+            Some(statement) => execute_statement(statement),
+            None => println!("Cannot execute this statement"),
         }
     }
 }
